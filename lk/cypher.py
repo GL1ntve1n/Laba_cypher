d14028527a2a5c7c3681d417b2f5c20d6748e954
Карпович Кирилл ИСТбд-22 <99400515+GL1ntve1n@users.noreply.github.com>
2024-03-24 00:28:31 +0400

    Update cypher.py

--- lk/cypher.py
+++ lk/cypher.py
@@ -2,16 +2,16 @@ secret_key = "keykey"
 
 #Алгоритм Виженера
 def vigenere(text: str, key: str, encrypt=True):
-    result = ""
-    for i in range(len(text)):
-        letter_n = ord(text[i])
-        key_n = ord(key[i % len(key)])
-        if encrypt:
-            value = (letter_n + key_n) % 1114112
-        else:
-            value = (letter_n - key_n) % 1114112
-        result += chr(value)
-    return result
+    result = ""  # Создание переменной для хранения результата шифрования или дешифрования
+    for i in range(len(text)):  # Итерация по каждому символу в тексте
+        letter_n = ord(text[i])  # Получение числового значения текущего символа текста
+        key_n = ord(key[i % len(key)])  # Получение числового значения символа ключа, который соответствует текущему символу текста
+        if encrypt:  # Если флаг encrypt установлен в True, то выполняется шифрование
+            value = (letter_n + key_n) % 1114112  # Вычисление нового числового значения символа после шифрования
+        else:  # Если флаг encrypt установлен в False, то выполняется дешифрование
+            value = (letter_n - key_n) % 1114112  # Вычисление нового числового значения символа после дешифрования
+        result += chr(value)  # Преобразование числового значения обратно в символ и добавление его к результату
+    return result  # Возвращение зашифрованного или расшифрованного текста
 
 #Функиця Шифрования
 def encrypt(text: str):
